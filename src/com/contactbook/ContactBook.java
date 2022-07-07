6cd253bf6246bc6a8316951d1dfbba9dc61b6afd
Jahnavi3003 <sujayithimulkj30.com>
2022-07-07 20:57:19 +0530

    Adding New Contact

--- src/com/contactbook/ContactBook.java
+++ src/com/contactbook/ContactBook.java
@@ -1,12 +1,14 @@
 package com.contactbook;
 
+import java.io.Console;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ContactBook {
 	
      public static void contact() {
     	 
-    	 System.out.println(" ADDRESS BOOK CREATED");
+    	 System.out.println(" ADDRESS BOOK ");
     	 ArrayList<String> cont = new ArrayList<String>();
     	 cont.add(" FirstName: ");
     	 cont.add(" LastName: ");
@@ -19,10 +21,31 @@ public class ContactBook {
     	 
     	 for(String phonebook:cont)
     		 System.out.println(phonebook);
+    	 
+    	 Scanner c = new Scanner(System.in);
+		  
+		  System.out.println("Enter new Contact Information ");
+		  System.out.println("Enter your First Name :");
+		  String s1 = c.next();
+		  System.out.println("Enter your Last Name :");
+		  String s2 = c.next();
+		  System.out.println("Enter your Address :");
+		  String s3 = c.next();
+		  System.out.println("Enter your City :");
+		  String s4 = c.next();
+		  System.out.println("Enter your State :");
+		  String s5 = c.next();
+		  System.out.println("Enter your ZipCode :");
+		  Double s6 = c.nextDouble();
+		  System.out.println("Enter your Phone :");
+		  Long s7 = c.nextLong();
+		  System.out.println("Enter your Email :");
+		  String s8 = c.next();
      }
      
      public static void main(String[] args) {
     	 contact();
+    	 System.out.println("New Contact Added Successfully");
     	 
      }
 }
