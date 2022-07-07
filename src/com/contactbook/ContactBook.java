12a6ed682918717287c80fdbcd1d1dabd1b306c0
Jahnavi3003 <sujayithimulkj30.com>
2022-07-07 21:04:14 +0530

    Editing Contact

--- src/com/contactbook/ContactBook.java
+++ src/com/contactbook/ContactBook.java
@@ -6,46 +6,39 @@ import java.util.ArrayList;
 
 public class ContactBook {
 	
-     public static void contact() {
-    	 
-    	 System.out.println(" ADDRESS BOOK ");
-    	 ArrayList<String> cont = new ArrayList<String>();
-    	 cont.add(" FirstName: ");
-    	 cont.add(" LastName: ");
-    	 cont.add(" Address: ");
-    	 cont.add(" City: ");
-    	 cont.add(" State: ");
-    	 cont.add(" ZipCode: ");
-    	 cont.add(" Phone: ");
-    	 cont.add(" Email: ");
-    	 
-    	 for(String phonebook:cont)
-    		 System.out.println(phonebook);
-    	 
-    	 Scanner c = new Scanner(System.in);
-		  
-		  System.out.println("Enter new Contact Information ");
-		  System.out.println("Enter your First Name :");
-		  String s1 = c.next();
-		  System.out.println("Enter your Last Name :");
-		  String s2 = c.next();
-		  System.out.println("Enter your Address :");
-		  String s3 = c.next();
-		  System.out.println("Enter your City :");
-		  String s4 = c.next();
-		  System.out.println("Enter your State :");
-		  String s5 = c.next();
-		  System.out.println("Enter your ZipCode :");
-		  Double s6 = c.nextDouble();
-		  System.out.println("Enter your Phone :");
-		  Long s7 = c.nextLong();
-		  System.out.println("Enter your Email :");
-		  String s8 = c.next();
+	public static void contact() {
+	   	 
+	   	 System.out.println(" ADDRESS BOOK ");
+	   	 ArrayList<String> cont = new ArrayList<String>();
+	   	 cont.add(" FirstName: Veccha");
+	   	 cont.add(" LastName: Jahnavi");
+	   	 cont.add(" Address: Javvarapeta");
+	   	 cont.add(" City: Machilipatnam");
+	   	 cont.add(" State: Andhra Pradesh");
+	   	 cont.add(" ZipCode: 520013");
+	   	 cont.add(" Phone: 8143822307");
+	   	 cont.add(" Email: sujayithimulkj30@gmail.com");
+	   	 
+	   	 
+	   	 for(String phonebook:cont)
+	   		 System.out.println(phonebook);
+	   	 
+	   	 System.out.println("Enter Name to Edit in Address Book");
+	   	 System.out.println();
+	   	 String newf="Sachin";
+	   	 String newl="Tendulkar";
+	   	 
+	   	 
+	   	 cont.set(0,"FirstName: "+newf);
+	   	cont.set(1,"LastName: " +newl);
+	   	 
+	   	 
+	   	 for(String phonebook:cont)
+	   		 System.out.println(phonebook);
+	    }
+	    
+	    public static void main(String[] args) {
+	   	 contact();
+	   	 
+	    }
      }
-     
-     public static void main(String[] args) {
-    	 contact();
-    	 System.out.println("New Contact Added Successfully");
-    	 
-     }
-}
