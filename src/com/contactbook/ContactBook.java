24ff0985a3775e29dcffe2883203a7e621e01e4e
Jahnavi3003 <sujayithimulkj30.com>
2022-07-09 17:07:01 +0530

    Adding Multiple Persons Contact

--- src/com/contactbook/ContactBook.java
+++ src/com/contactbook/ContactBook.java
@@ -4,39 +4,77 @@ import java.io.Console;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class ContactBook {
-	
-	public static void contact() {
-	   	 
-	   	 System.out.println(" ADDRESS BOOK ");
-	   	 ArrayList<String> cont = new ArrayList<String>();
-	   	 cont.add(" FirstName: Veccha");
-	   	 cont.add(" LastName: Jahnavi");
-	   	 cont.add(" Address: Javvarapeta");
-	   	 cont.add(" City: Machilipatnam");
-	   	 cont.add(" State: Andhra Pradesh");
-	   	 cont.add(" ZipCode: 520013");
-	   	 cont.add(" Phone: 8143822307");
-	   	 cont.add(" Email: sujayithimulkj30@gmail.com");
-	   	 
-	   	 
-	   	 for(String phonebook:cont)
-	   		 System.out.println(phonebook);
-	   	 
-	   	 
-	   	 System.out.println();
-	   	 
-	   	 
-	   	 cont.remove(0);
-    	cont.remove(0);
-	   	 
-	   	 
-	   	 for(String phonebook:cont)
-	   		 System.out.println(phonebook);
-	    }
-	    
-	    public static void main(String[] args) {
-	   	 contact();
-	   	 
-	    }
-     }
+class ContactBook {
+    private String firstName, lastName, address, city, state, email;
+    private int zip;
+    private long phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return ("First name: " + firstName + "\n Last name: " + lastName + "\n Address: " + address + "\n city: " + city
+                + "\n state: " +state + "\n email: " + email + "\n zip: " + zip + "\n phone number:" + phoneNumber + "");
+    }
+}
